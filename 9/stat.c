00bf4980930d382b9c652cfc49a3b215fa1a3250
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2020-12-17 15:18:16 +0530

    Add files via upload

---
+++ 9/stat.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+
+int main(int argc,char *argv[])
+{
+	struct stat statbuffer;
+	if( lstat (argv[1],&statbuffer)==-1)
+	{
+		printf("Couldn't stat file");
+		exit(0);
+	}
+	printf("File name: %s\n",argv[1]);
+	printf("Inode no : %ld\n",statbuffer.st_ino);
+	printf("Type and permission:  %o\n",statbuffer.st_mode );
+	printf("No of links: %ld\n",statbuffer.st_nlink);
+	printf("UID : %d\n",statbuffer.st_uid);
+	printf("GID : %d\n",statbuffer.st_gid);
+	
+	exit(0);
+	
+}
